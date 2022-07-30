2e916021a6ad2f6c999683cba4e668eaa2181ee6
Juan Cassiano <juancassiano@hotmail.com>
2022-07-29 23:56:40 -0300

    :tada: first commit of script to linux server Apache

---
+++ iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando arquivos da aplicação"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linus-site-dio
+cp -R * /var/www/html/
+
+
+
